78cb039cda43f51aeed9c72c86d47ac8fcf181bf
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 14:13:50 +0200

    Thu 05 Oct 2023 02:10:42 PM CEST

---
+++ build/c.stm32/out/fpsin.h
@@ -0,0 +1,8 @@
+#define _FPSIN_H
+// x-scale: 1:1000 (0-6283 <=> 0-2PI), y-scale:1:1000 (-1000/+1000 <=> -1/+1)
+extern sb2 fpsin(sb2 i);
+
+#define FPSININIT fiosAdd("sin",IOSCALLBACK(fpsin),1,2,2)
+
+
+
