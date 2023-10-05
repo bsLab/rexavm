cee2ddb0209ec2b2c3bfd2fd4a524ed8af32dfd3
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 14:12:50 +0200

    Thu 05 Oct 2023 02:10:42 PM CEST

---
+++ build/c/out/fprelu.h
@@ -0,0 +1,7 @@
+#define _FPRELU_H
+// x-scale: 1:1000 (0-6283 <=> 0-2PI), y-scale:1:1000 (-1000/+1000 <=> -1/+1)
+extern sb2 fprelu(sb2 i);
+
+#define FPRELUINIT fiosAdd("relu",IOSCALLBACK(fprelu),1,2,2)
+
+
