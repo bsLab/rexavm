fedc7159f38ccd81db85f5c89eacb3be252f3a85
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 14:10:52 +0200

    Thu 05 Oct 2023 02:10:42 PM CEST

---
+++ build/stm32f103rexavm/vmloop.h
@@ -0,0 +1,24 @@
+#ifndef __VMLOOP_H
+#define __VMLOOP_H
+#include "vmcode.h"
+#define DECODE(word) (word&0x80?word&0x7F:word&0x40?OP_TWOLIT:OP_LIT)
+  #define DISPATCH()  ; if ((--steps)==0) goto schedule; break
+#if VMLOOP==SWITCHCASEGOTO
+  #define DISPATCH()  ; if ((--steps)==0) goto schedule; goto dispatch
+#endif
+#if VMLOOP==FUNCTABLE
+  #define DISPATCH()  ; if ((--steps)==0) goto schedule;
+#endif
+#define EXTENDSIGN(v) (v&0x20?v|0xC0:v)
+
+// Main VM loop executing code.
+// Code: CS or any other buffer, pc: CS offset address, steps: maximal number of steps
+// Returns current (next) pc value; if a negative value is returned, then the current op is suspended (e.g., sleep)
+// The vmevent.timeout fields > 0 is the next time when the vmloop may be reentered.
+// If (ub4) vmevent.v is not null, the vmloop may be reentered only if *v==vmevent.c is satisfied
+// if an error occured, vmerror contains the error code
+EXTERN sb2 vmloop(ub1* code, ub2 pc, ub2 steps);
+#endif
+
+
+
