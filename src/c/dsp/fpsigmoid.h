878a1b0e5f573a1dbc0953dc1dfceaf2c82d955c
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 14:06:57 +0200

    Thu 05 Oct 2023 02:03:47 PM CEST

---
+++ src/c/dsp/fpsigmoid.h
@@ -0,0 +1,10 @@
+#ifndef _FPSIGMOID_H
+#define _FPSIGMOID_H
+
+ // y scale 1:1000 [0,1], x scale 1:1000
+extern sb2 fpsigmoid(sb2 x);
+
+#define FPSIGMOIDINIT   fiosAdd("sigmoid",IOSCALLBACK(fpsigmoid),1,2,2)
+
+#endif
+
