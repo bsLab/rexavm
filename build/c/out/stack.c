844527d125f7c75855a5eab003f92a373068ec89
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 14:12:22 +0200

    Thu 05 Oct 2023 02:10:42 PM CEST

---
+++ build/c/out/stack.c
@@ -0,0 +1,16 @@
+
+#include "config.h"
+#include "standard.h"
+#include "vm.h"
+#include "stack.h"
+
+// Data Stack
+sw1 DS[16000];
+// Return Stack
+sw1 RS[4000];
+// Loop stack
+sw1 FS[4000];
+uw1 dstop=0,rstop=0,fstop=0;
+
+
+
